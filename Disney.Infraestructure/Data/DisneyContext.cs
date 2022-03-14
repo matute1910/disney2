8f10eb34a79249b5f48dba8014055538baddc12c
Matias <78965121+matute1910@users.noreply.github.com>
2022-03-14 17:01:33 -0300

    Add files via upload

---
+++ Disney.Infraestructure/Data/DisneyContext.cs
@@ -0,0 +1,30 @@
+﻿using Disney.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Disney.Infraestructure.Data
+{
+    public partial class DisneyContext :DbContext
+     {
+        public DisneyContext()
+        {
+        }
+
+        public DisneyContext(DbContextOptions<DisneyContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Character> Character { get; set; }
+        public virtual DbSet<Gender> Gender { get; set; }
+        public virtual DbSet<MovieOrSeries> MovieOrSeries { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
