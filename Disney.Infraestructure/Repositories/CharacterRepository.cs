d6a3e69b21457090f95edf89525448cb8662fa79
Matias <78965121+matute1910@users.noreply.github.com>
2022-03-14 17:03:23 -0300

    Add files via upload

---
+++ Disney.Infraestructure/Repositories/CharacterRepository.cs
@@ -0,0 +1,36 @@
+﻿using Disney.Core.Entities;
+using Disney.Core.Interfaces;
+using Disney.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disney.Infraestructure.Repositories
+{
+    public class CharacterRepository : BaseRepository<Character>, ICharacterRepository
+    {
+        public CharacterRepository(DisneyContext context) : base(context) { }
+
+        public async Task<IEnumerable<Character>> GetListCaracter(string name, string image)
+        {
+            return await _entities.Where(x => x.Name == name && x.Image == image).ToListAsync();       
+        }
+
+        public async Task<IEnumerable<Character>> GetByName(string name)
+        {
+            return await _entities.Where(x => x.Name == name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Character>> GetByAge(int age)
+        {
+            return await _entities.Where(x => x.Age == age).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Character>> GetByIdMovie(int idMovie)
+        {
+            return await _entities.Where(x => x.MoviesOrAssociatedSeries.Any(z => z.Id == idMovie)).ToListAsync();
+        }
+    }
+
+}
---
+++ Disney.Infraestructure/Repositories/MovieOrSeriesRepository.cs
@@ -0,0 +1,31 @@
+﻿using Disney.Core.Entities;
+using Disney.Core.Interfaces;
+using Disney.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disney.Infraestructure.Repositories
+{
+    public class MovieOrSeriesRepository : BaseRepository<MovieOrSeries>, IMovieOrSeriesRepository
+    {
+        public MovieOrSeriesRepository(DisneyContext context) : base(context) { }
+
+        public Task<IEnumerable<Gender>> GetGender(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<MovieOrSeries>> GetListMovieOrSeries(string tittle, string image, DateTime creationDate)
+        {
+            return await _entities.Where(x => x.Tittle == tittle && x.Image == image && x.CreationDate == creationDate).ToListAsync();
+        }
+
+        public Task<IEnumerable<MovieOrSeries>> GetNameMovieOrSeries(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
